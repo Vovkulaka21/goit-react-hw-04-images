3a78f11bec7a6a62f3b0b093b84af0e37ed5bc41
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2024-02-28 16:52:21 +0200

    Update Searchbar.jsx

--- src/components/Serachbar/Searchbar.jsx
+++ src/components/Serachbar/Searchbar.jsx
@@ -2,25 +2,18 @@ import css from './Searchbar.module.css';
 
 import { useState } from 'react';
 
-
-const Searchbar = ({onSubmit}) => {
-
+const Searchbar = ({ onSubmit }) => {
   const [search, setSearch] = useState('');
-  
+
   const handleChange = ({ target }) => {
-    const {value} = target;
-    setSearch({
-      search: value,
-    });
+    setSearch(target.value);
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
-    onSubmit({...search});
-    setSearch({
-      search: '',
-    });
+    onSubmit({ search });
   };
+
   return (
     <header className={css.searchbar}>
       <form onSubmit={handleSubmit} className={css.searchbar_form}>
@@ -40,50 +33,6 @@ const Searchbar = ({onSubmit}) => {
       </form>
     </header>
   );
-}
-
-/* class Searchbar extends Component {
-  state = {
-    search: '',
-  };
-
-  handleChange = ({ target }) => {
-    const {value} = target;
-    this.setState({
-      search: value,
-    });
-  };
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit({...this.state});
-    this.setState({
-      search: '',
-    });
-  };
-
-  render() {
-
-    return (
-      <header className={css.searchbar}>
-        <form onSubmit={this.handleSubmit} className={css.searchbar_form}>
-          <button type="submit" className={css.searchbar_form_button}>
-            <span className={css.searchbar_form_button_label}>Search</span>
-          </button>
-
-          <input
-            value={this.state.search}
-            onChange={this.handleChange}
-            className={css.searchbar_form_input}
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-          />
-        </form>
-      </header>
-    );
-  }
-} */
+};
 
 export default Searchbar;
