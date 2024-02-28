96aff26e318bb24113d48cdd6950792b6fda637e
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2024-02-28 15:16:06 +0200

    Update Searchbar.jsx

--- src/components/Serachbar/Searchbar.jsx
+++ src/components/Serachbar/Searchbar.jsx
@@ -1,8 +1,48 @@
 import css from './Searchbar.module.css';
 
-import { Component } from 'react';
+import { useState } from 'react';
 
-class Searchbar extends Component {
+
+const Searchbar = ({onSubmit}) => {
+
+  const [search, setSearch] = useState('');
+  
+  const handleChange = ({ target }) => {
+    const {value} = target;
+    setSearch({
+      search: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit({...search});
+    setSearch({
+      search: '',
+    });
+  };
+  return (
+    <header className={css.searchbar}>
+      <form onSubmit={handleSubmit} className={css.searchbar_form}>
+        <button type="submit" className={css.searchbar_form_button}>
+          <span className={css.searchbar_form_button_label}>Search</span>
+        </button>
+
+        <input
+          value={search}
+          onChange={handleChange}
+          className={css.searchbar_form_input}
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </form>
+    </header>
+  );
+}
+
+/* class Searchbar extends Component {
   state = {
     search: '',
   };
@@ -44,6 +84,6 @@ class Searchbar extends Component {
       </header>
     );
   }
-}
+} */
 
 export default Searchbar;
