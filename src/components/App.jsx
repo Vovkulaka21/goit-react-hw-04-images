961abdd62f3769b4c251f4bdfbcdbaf741fe1c07
Vovkulaka21 <110464093+Vovkulaka21@users.noreply.github.com>
2024-02-29 13:21:29 +0200

    modal

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -21,23 +21,22 @@ const App = () => {
 
   useEffect(() => {
     const fetchImages = async () => {
-      if (search) {
-        try {
-          setLoading(true);
-          const { data } = await searchImages(search, page);
-          const { hits } = data;
-          setImages(prevImages =>
-            hits?.length ? [...prevImages, ...hits] : prevImages
-          );
-        } catch (error) {
-          setError(error.message);
-        } finally {
-          setLoading(false);
-        }
+      try {
+        setLoading(true);
+        const { data } = await searchImages(search, page);
+        const { hits } = data;
+        setImages(prevImages =>
+          hits?.length ? [...prevImages, ...hits] : prevImages
+        );
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
       }
     };
-
-    fetchImages();
+    if (search) {
+      fetchImages();
+    }
   }, [search, page]);
 
   const handleSearch = ({ search }) => {
@@ -78,85 +77,5 @@ const App = () => {
     </>
   );
 };
-
-/* 
-export class App extends Component {
-  state = {
-    search: '',
-    images: [],
-    page: 1,
-    loading: false,
-    error: null,
-    modalOpen: false,
-    largeImage: ''
-  };
-
-  async componentDidUpdate(_, prevState) {
-    const { page, search } = this.state;
-    if (search && (search !== prevState.search || page !== prevState.page)) {
-      this.setState({
-        loading: true,
-      });
-      try {
-        const { data } = await searchImages(search, page);
-        const { hits } = data;
-        this.setState(({ images }) => ({
-          images: hits?.length ? [...images, ...hits] : images,
-        }));
-      } catch (error) {
-        this.setState({
-          error: error.message,
-        });
-      } finally {
-        this.setState({ loading: false });
-      }
-    }
-  }
-
-  handleSearch = ({ search }) => {
-    this.setState({
-      search,
-      images: [],
-      page: 1,
-    });
-  };
-
-  pageRiser = () => {
-    this.setState(({ page }) => ({ page: page + 1 }));
-  };
-
-showModal = (largeImageURL) => 
-this.setState({
-  modalOpen: true,
-  largeImage: largeImageURL
-});
-
-closeModal = () => {
-  this.setState({
-    modalOpen: false,
-  });
-}
-
-  render() {
-    return (
-      <>
-        <Searchbar onSubmit={this.handleSearch} />
-        <div className={css.app_box}>
-          {this.state.error && <p>{this.state.error}</p>}
-          {Boolean(this.state.images.length) && (
-            <ImageGallery>
-              <ImageGalleryItem showModal={this.showModal} images={this.state.images} />
-            </ImageGallery>
-          )}
-          {this.state.loading && <Loader />}
-          {Boolean(this.state.images.length) && (
-            <Button onClick={this.pageRiser} />
-          )}
-        </div>
-        {this.state.modalOpen && <Modal close={this.closeModal}><img src={this.state.largeImage} alt="img" /></Modal>}
-      </>
-    );
-  }
-} */
 
 export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,37 +1,28 @@
 import css from './Modal.module.css';
 
-import { Component } from 'react';
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 
 const modalRoot = document.getElementById('modal-root');
 
-class Modal extends Component {
+const Modal = ({ close, children }) => {
+  useEffect(() => {
+    document.addEventListener('keydown', closeModal);
+    return () => document.removeEventListener('keydown', closeModal);
+  }, []);
 
-
-  componentDidMount() {
-    document.addEventListener("keydown", this.closeModal)
-  }
-
-  componenWillUnmount() {
-    document.removeEventListener("keydown", this.closeModal)
-  }
-
-  closeModal = ({target, currentTarget, code}) => {
-
-    if(target === currentTarget || code === "Escape") {
-      this.props.close()
+  const closeModal = ({ target, currentTarget, code }) => {
+    if (target === currentTarget || code === 'Escape') {
+      close();
     }
-  }
-  
-  render() {
-    const { children} = this.props;
-    return createPortal(
-      <div onClick={this.closeModal} className={css.overlay}>
-        <div className={css.modal}>{children}</div>
-      </div>,
-      modalRoot
-    );
-  }
-}
+  };
+
+  return createPortal(
+    <div onClick={closeModal} className={css.overlay}>
+      <div className={css.modal}>{children}</div>
+    </div>,
+    modalRoot
+  );
+};
 
 export default Modal;
